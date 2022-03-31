b2326895a8fde31e9eb6e75a070797987abc4f97
Ramdani Official <100227892+Ramdaniofficial@users.noreply.github.com>
2022-03-31 13:19:01 +0700

    Update mess.js

--- message/mess.js
+++ message/mess.js
@@ -1,6 +1,6 @@
 module.exports.mess = {
 	        regist: '─ 「 *NOT REGISTERED* 」 ─\n\n*ENGLISH*\n_YOU HAVE NOT REGISTERED ON THE BOTTOM OF I DECK, TYPE .verify FOR RUQYAH_\n\n*INDONESIA*\n_LUH BELUM DAFTAR DI BOT GUA DEX, KETIK .daftar UNTUK DI RUQYAH_',
-		    dewasa: '─ 「 *WARNING* 」 ─\n\n*ENGLISH*\n_not allowed for minors, if you are an adult, please verify first, type .my18_\n\n*INDONESIA*\n_dilarang untuk anak di bawah umur, jika Anda sudah dewasa, silahkan verifikasi terlebih dahulu, ketik .saya18',
+		    dewasa: '─ 「 *WARNING* 」 ─\n\n*ENGLISH*\n_not allowed for minors, if you are an adult, please verify first, type .my18_\n\n*INDONESIA*\n_dilarang untuk anak di bawah umur, jika Anda sudah dewasa, silahkan verifikasi terlebih dahulu, ketik .saya18_',
 		    waitregist: '*ENGLISH*\n_WAIT A MINUTE DECK_\n\n*INDONESIA*\n_TUNGGU SEBENTAR DEK_',
 	        wait: '_Wait Process!..._',
 			success: '_Done, don t forget to subscribe_ : https://youtube.com/channel/UCB157jomCne961WzYHpG4gg',
